f5bf44e2be82ceb0f667ede7e1b1ac73ddd490b2
unknown <joaquimmigueldecristo@gmail.com>
2025-11-17 23:25:36 +0100

    first commit5

--- assets/js/components/menu/home.js
+++ assets/js/components/menu/home.js
@@ -1,113 +1,29 @@
-import get_template from '../../components/get_template.js'
+import get_template from "../../components/get_template.js";
 
 export default {
-    data: function () {
-        return {
-            title: "home",
-            activo: false, 
-            activo1: true,
-            activo2: false,
-            activo3: false,
-            activo4: false,
-            activo5: false,
-        }
-    },
+  data: function () {
+    return {
+      title: "home",
+      activo: false,
+      activo1: true,
+      activo2: false,
+      activo3: false,
+      activo4: false,
+      activo5: false,
+    };
+  },
 
-    methods: {
- 
-        mostrarMenu() {  
-            this.activo = !this.activo; 
-        },
-      
- 
-        homeMenu() { 
-                this.activo5 = false,
-                this.activo4 = false,
-                this.activo3 = false,
-                this.activo2 = false,
-                this.activo1 = true,
-                this.activo = !this.activo; 
-        },
+  methods: {},
 
-        sobreMenu() {
-            this.activo5 = false,
-            this.activo4 = false,
-            this.activo3 = false,
-            this.activo2 = true,
-            this.activo1 = false
-            this.activo = !this.activo; 
-        },
+  async mounted() {
+    const toggle = document.getElementById("menu-toggle");
+    const mobileNav = document.getElementById("mobile-nav");
 
-        servicoMenu() {
-            this.activo5 = false,
-            this.activo4 = false,
-            this.activo3 = true,
-            this.activo2 = false,
-            this.activo1 = false,
-            this.activo = !this.activo; 
-        },
-
-        galeriaMenu() {
-            this.activo5 = false,
-            this.activo4 = true,
-            this.activo3 = false,
-            this.activo2 = false,
-            this.activo1 = false,
-            this.activo = !this.activo; 
-        },
-
-        contactoMenu() {
-            this.activo5 = true,
-            this.activo4 = false,
-            this.activo3 = false,
-            this.activo2 = false,
-            this.activo1 = false,
-            this.activo = !this.activo; 
-        },
-
-         
-      
-    },
-
-     async mounted() {
-          /**
-   * Mobile nav toggle
-   */
-  const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
-
-  function mobileNavToogle() {
-    document.querySelector('body').classList.toggle('mobile_nav_active');
-    mobileNavToggleBtn.classList.toggle('bi-list');
-    mobileNavToggleBtn.classList.toggle('bi-x');
-  }
-  if (mobileNavToggleBtn) {
-    mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
-  }
-
-  /**
-   * Hide mobile nav on same-page/hash links
-   */
-  document.querySelectorAll('#navmenu a').forEach(navmenu => {
-    navmenu.addEventListener('click', () => {
-      if (document.querySelector('.mobile_nav_active')) {
-        mobileNavToogle();
-      }
+    toggle.addEventListener("click", () => {
+      mobileNav.style.display =
+        mobileNav.style.display === "block" ? "none" : "block";
     });
+  },
 
-  });
-
-  /**
-   * Toggle mobile nav dropdowns
-   */
-  document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
-    navmenu.addEventListener('click', function(e) {
-      e.preventDefault();
-      this.parentNode.classList.toggle('active');
-      this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
-      e.stopImmediatePropagation();
-    });
-  });
-   },
-    
-    template: await get_template('./assets/js/components/menu/home')
-}
+  template: await get_template("./assets/js/components/menu/home"),
+};
